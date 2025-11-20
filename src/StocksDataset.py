5c2a1fa23b3786f620d5cb3c6e4205f34fcbecc2
Nasty_Hedgeh0g <niazimohammada@gmail.com>
2025-11-20 22:48:02 +0330

    Created the dataset class that would be fed to the model. it downloads the data from Yahoo Finance or loads from disk

--- src/StocksDataset.py
+++ src/StocksDataset.py
@@ -1,13 +1,15 @@
 import torch
 import yfinance as f
 import pandas as pd
-from torch.utils.data import dataset
+from torch.utils.data import Dataset
 
 
-class StocksDataset:
-    def __init__(self, sTickerSymbol=None, sPath=None, bDownloadFallBack=False):
 
-        df = None
+class StocksDataset(Dataset):
+    def __init__(self, sTickerSymbol: str = None, sPath: str = None, iSequenceLength: int = 60):
+
+        df          = None
+        self.sl     = iSequenceLength
 
         if sTickerSymbol:
             t = f.Ticker(sTickerSymbol)
@@ -15,27 +17,40 @@ class StocksDataset:
             df = df.reset_index()
             del df['Dividends']
             del df['Stock Splits']
-            df['Date'] = df['Date'].str.split(' ').str[0]
+            # df['Date'] = df['Date'].str.split(' ').str[0]
+            df["Date"] = df["Date"].dt.strftime("%Y-%m-%d")
+
         
         elif sPath:
             from os.path import exists
 
             if not exists(sPath):
-                print(f"No such file {sPath}")
-                return
+                    raise FileNotFoundError(f"No such file << {sPath} >> exists - download fallback was turned off")
+            
             df = pd.read_csv(sPath)
             if 'Dividends'    in df:  del df['Dividends']
             if 'Stock Splits' in df:  del df['Stock Splits']
 
             df['Date'] = df['Date'].str.split(' ').str[0]
 
+        self.data = df[["Open", "Close", "Volume"]].values.astype(float)
+        self.date = df['Date'].tolist()
         
-        print(df)
 
     def __len__(self):
-        pass
-    def __getitem__(self, iIDX):
-        pass
+        return len(self.data) - self.sl
+    def __getitem__(self, IDX):
+        '''
+            Shapes the data into:
 
+                    ---> d: [sequence_length]
+                    ---> x: (sequence_length, 3)
+                    ---> y: (1)
+        '''
 
-StocksDataset(sPath="data.csv")
+        if IDX < 0: raise IndexError(f"StocksDataset.__getitem__() -> index={IDX} out of range")
+
+        d = self.date[IDX: IDX + self.sl]
+        x = torch.tensor(self.data[IDX: IDX + self.sl],    dtype=torch.float32) #To train over a sequence
+        y = torch.tensor(self.data[IDX + self.sl][1],      dtype=torch.float32) #To predict the next day's market
+        return d, x, y
